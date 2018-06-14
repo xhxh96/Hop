f7837054a6109e10b6d60b75fcdd9816adb8db4c
Xian Hao <xianhao@u.nus.edu>
2018-06-14 21:30:15 +0800

    Initial Commit

---
+++ Hop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hop
+//
+//  Created by XIANHAO on 14/6/18.
+//  Copyright © 2018 NUS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
