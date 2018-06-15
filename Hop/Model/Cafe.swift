79a577020507328e7804308309313fd0409f3908
macOS <macos@macoss-imac.local>
2018-06-15 12:07:15 +0800

    Version 1.1 - Created models for search results and cafe page

--- Hop/Model/Cafe.swift
+++ Hop/Model/Cafe.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit
+
+enum Amenities {
+    case cardPayment(Bool)
+    case wifi(Bool)
+    case power(Bool)
+    case parking(Bool)
+    case transit(Bool)
+    case halal(Bool)
+    case vegetarian(Bool)
+    case water(Bool)
+    case contact(Bool)
+    case booking(Bool)
+}
+
+class Cafe {
+    var images: [UIImage]
+    var name: String
+    var address: String
+    var bloggerReview: Int
+    var hopperReview: Int
+    var priceRange: Int
+    var amenities: [Amenities]
+    var locationX: Double
+    var locationY: Double
+    
+    init(images: [UIImage], name: String, address: String, bloggerReview: Int, hopperReview: Int, priceRange: Int, amenities: [Amenities], locationX: Double, locationY: Double) {
+        self.images = images
+        self.name = name
+        self.address = address
+        self.bloggerReview = bloggerReview
+        self.hopperReview = hopperReview
+        self.priceRange = priceRange
+        self.amenities = amenities
+        self.locationX = locationX
+        self.locationY = locationY
+    }
+}
--- Hop/Model/SearchResult.swift
+++ Hop/Model/SearchResult.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+class SearchResult {
+    var name: String
+    var address: String
+    var description: String
+    
+    init(name: String, address: String, description: String) {
+        self.name = name
+        self.address = address
+        self.description = description
+    }
+}
