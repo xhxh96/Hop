539288edd9a04711d6b67e5b07ee9b8fe1144ea8
Xian Hao <xianhao@u.nus.edu>
2018-09-05 15:09:09 +0800

    RC Version 2.1 - Fixed aspect ratio issue of profile photo

--- Hop/Controller/AppInfoViewController.swift
+++ Hop/Controller/AppInfoViewController.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 class AppInfoViewController: UIViewController {
+    
+    @IBOutlet weak var appIcon: UIImageView!
     @IBOutlet weak var buildTextField: UILabel!
     @IBOutlet weak var versionTextField: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tap(_:)))
+        tap.numberOfTapsRequired = 4
+        view.addGestureRecognizer(tap)
         setupLabel()
     }
 
@@ -25,5 +30,20 @@ class AppInfoViewController: UIViewController {
     
     @IBAction func closeButtonTapped(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tap(_ sender: UIGestureRecognizer) {
+        UIView.animate(withDuration: 5, animations: {
+            let scaleTransform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            let rotateTransform = CGAffineTransform(rotationAngle: .pi)
+            
+            let transform = scaleTransform.concatenating(rotateTransform)
+            self.appIcon.transform = transform
+            
+        }) { (_) in
+            UIView.animate(withDuration: 2, animations: {
+                self.appIcon.transform = CGAffineTransform.identity
+            })
+        }
     }
 }
